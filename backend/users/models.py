8a211fb6e4dab257503fb446080890491a196897
Manuel Palacios <manpalpin@alum.us.es>
2025-03-05 20:19:00 +0100

    feat: formulario de preferencias básico
    Se ha añadido el código de backend para obtener las preferencias de los usuarios de manera muy básica

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Usuario(models.Model):
@@ -11,4 +12,22 @@ class Usuario(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=15)
 
+    adventure = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    culture = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    sports = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    gastronomy = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    nightlife = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    music = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+    def get_preferences(self):
+        preferences = {}
+        preferences['adventure'] = self.adventure
+        preferences['culture'] = self.culture
+        preferences['sports'] = self.sports
+        preferences['gastronomy'] = self.gastronomy
+        preferences['nightlife'] = self.nightlife
+        preferences['music'] = self.music
+        return preferences
