d0259714b80598cee5006b12defa9583faf194b9
Rafael Cabello <rafcabran@alum.us.es>
2025-03-06 08:25:11 +0100

    Arreglos en creación de reserva

--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -9,7 +9,12 @@ class Booking(models.Model):
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants = models.PositiveIntegerField(default=1)
-    total_price = models.FloatField()
+    price = models.FloatField(
+        help_text="Precio por persona de la reserva. Debería ser el mismo que el de la experiencia, de los rangos de precio que haya."
+    )
+    total_price = models.FloatField(
+        help_text="Precio total de la reserva. Precio por persona multiplicado por el número de participantes."
+    )
     booking_date = models.DateField()
     experience_date = models.DateField()
     cancellable = models.BooleanField()
@@ -20,10 +25,11 @@ class Booking(models.Model):
             ('CANCELLED', 'Cancelada'),
             ('CONFIRMED', 'Confirmada'),
         ),
-        default='PENDING'
+        default='PENDING',
+        help_text="Estado de la reserva. Comienza en 'PENDING' y se actualiza según se asigne la experiencia."
     )
 
-    user = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='reservas')
+    user = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='usuario')
     experience = models.ForeignKey(Experience, on_delete=models.CASCADE, related_name='reservas')
 
     
