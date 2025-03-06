2a77a35d2e72609a7cd23844e91982a070a2cb1b
RamonVG8 <ramvergar@alum.us.es>
2025-03-06 18:47:20 +0100

    conflicts fixed

--- backend/experiences/views.py
+++ backend/experiences/views.py
@@ -10,7 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-from backend.experiences.forms import UserPreferencesForm
+from .forms import UserPreferencesForm
 from experiences.models import Experience
 from experiences.serializers import ExperienceSerializer
 from users.models import Usuario
