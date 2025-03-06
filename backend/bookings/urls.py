1134d1e4ddd1b553ade31fed15c383eeeca9a2d1
Rafael Cabello <rafcabran@alum.us.es>
2025-03-06 07:41:22 +0100

    Obtener reservas usuario

--- backend/bookings/urls.py
+++ backend/bookings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from bookings.views import crear_reserva, obtener_reserva
+from bookings.views import crear_reserva, obtener_reserva, obtener_reservas_usuario
 
 urlpatterns = [
     path('crear-reserva/', crear_reserva, name='crear_reserva'),
     path('obtener-reserva/<uuid:id>/', obtener_reserva, name='obtener_reserva'),
+    path('users/<uuid:user_id>/', obtener_reservas_usuario, name='obtener_reservas_usuario'),
 ]
