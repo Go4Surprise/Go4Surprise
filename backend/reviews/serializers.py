4bd469ffd1f1e17b7d77cce5c288b03b4c56cad1
Pablo Rodríguez Sánchez <pabrodsan@alum.us.es>
2025-03-08 20:58:30 +0100

    Get Reviews

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -4,24 +4,31 @@ from experiences.models import Experience
 from users.models import Usuario
 from .models import Reviews
 
+
 class CreateReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reviews
-        fields = ['puntuacion', 'comentario', 'user', 'experience']
-        
+        fields = ["puntuacion", "comentario", "user", "experience"]
+
     def validate_puntuacion(self, value):
         if value < 0 or value > 5:
             raise serializers.ValidationError("La puntuación ha de estar entre 0 y 5")
         return value
-    
+
     def validate(self, data):
-        user_id = data.get('user').id
-        experience_id = data.get('experience').id
-        
+        user_id = data.get("user").id
+        experience_id = data.get("experience").id
+
         if not Usuario.objects.filter(id=user_id).exists():
             raise serializers.ValidationError("El usuario seleccionado no existe")
-        
+
         if not Experience.objects.filter(id=experience_id).exists():
             raise serializers.ValidationError("La experiencia seleccionada no existe")
-        
+
         return data
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reviews
+        fields = "__all__"
