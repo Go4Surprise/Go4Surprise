597134d5112c2f3b8de5441ff5d78ab9009bbe04
gonzalodom11 <jgdominguezmoreno@gmail.com>
2025-03-24 20:09:42 +0100

    fix: duplicated file detection

---
+++ Go4SurpriseFrontend/app/PasswordResetConfirm.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import axios from "axios";
+import { BASE_URL } from '../constants/apiUrl';
+import { useLocalSearchParams, useRouter } from "expo-router";
+
+
+const PasswordResetConfirm = () => {
+    const { uidb64, token } = useLocalSearchParams(); 
+    const router = useRouter();
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${BASE_URL}/users/reset/${uidb64}/${token}/`, { password });
+      setMessage("Password reset successful. Redirecting...");
+      setTimeout(() => router.push("/LoginScreen"), 3000);
+    } catch (error) {
+      setMessage("Error resetting password.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Enter New Password</h2>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="password" 
+          placeholder="New password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          required 
+        />
+        <button type="submit">Reset Password</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+
+export default PasswordResetConfirm;
